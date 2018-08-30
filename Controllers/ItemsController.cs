a435b4bf1ba67398f62c0b9282132db0410c85e8
diaaaaaa <31830007+diaaaaaa@users.noreply.github.com>
2018-08-30 11:03:28 +0200

    Add files via upload

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnularAndDonetCoreProjet.Controllers
+{
+    [Route("api/[controller]")]
+    public class ItemsController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Laptop", "Smart TV", "Smart Phone", "Tablet" };
+        }
+
+       
+    }
+}
